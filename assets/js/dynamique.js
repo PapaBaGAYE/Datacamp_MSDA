84fa827c26d4c68d938ab307ced60b8ce97107ab
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-06-26 15:51:51 +0000

    Ajout initial

---
+++ assets/js/dynamique.js
@@ -0,0 +1,16 @@
+// document.getElementsByClassName("sidebar").style.color = "red"
+// document.getElementById("myDIV").style.display = "none";
+$(function(){
+    // alert("hello")
+    // $("div").hover(function(){
+    //     // $(this).addClass("visible")
+    //     console.log($(this).attr('class'))
+    // })
+    // text = $(document).html()
+    // alert(text)
+    console.log($(".sidebar"))
+    // $('body').css({"background-color": "red!important"})
+})
+
+
+
